e80de810eb811940345bc03e767922bb1b95d59d
Leah Barnes <johnson.leahe@gmail.com>
2019-12-30 20:52:55 -0500

    update index.js update generateHTML.js

--- generateHTML.js
+++ generateHTML.js
@@ -25,7 +25,7 @@ const colors = {
   }
 };
 
-function generateHTML(githubData) {
+function generateHTML(githubData, stars, color) {
   return `<!DOCTYPE html>
 <html lang="en">
    <head>
@@ -52,7 +52,7 @@ function generateHTML(githubData) {
          height: 100%;
          }
          .wrapper {
-         background-color: ${colors[data.color].wrapperBackground};
+         background-color: ${colors[color].wrapperBackground};
          padding-top: 100px;
          }
          body {
@@ -94,8 +94,8 @@ function generateHTML(githubData) {
          display: flex;
          justify-content: center;
          flex-wrap: wrap;
-         background-color: ${colors[data.color].headerBackground};
-         color: ${colors[data.color].headerColor};
+         background-color: ${colors[color].headerBackground};
+         color: ${colors[color].headerColor};
          padding: 10px;
          width: 95%;
          border-radius: 6px;
@@ -106,7 +106,7 @@ function generateHTML(githubData) {
          border-radius: 50%;
          object-fit: cover;
          margin-top: -75px;
-         border: 6px solid ${colors[data.color].photoBorderColor};
+         border: 6px solid ${colors[color].photoBorderColor};
          box-shadow: rgba(0, 0, 0, 0.3) 4px 1px 20px 4px;
          }
          .photo-header h1, .photo-header h2 {
@@ -149,8 +149,8 @@ function generateHTML(githubData) {
          .card {
            padding: 20px;
            border-radius: 6px;
-           background-color: ${colors[data.color].headerBackground};
-           color: ${colors[data.color].headerColor};
+           background-color: ${colors[color].headerBackground};
+           color: ${colors[color].headerColor};
            margin: 20px;
          }
          
@@ -224,6 +224,4 @@ function generateHTML(githubData) {
       </html>`
 }
 
-module.exports = {
-  generateHTML: generateHTML
-};
+module.exports = generateHTML;
--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const inquirer = require('inquirer');
 const pdf = require('html-pdf');
 const genHTML = require('./generateHTML.js');
 // const readFileAsync = util.promisify(fs.readFile);
-
+let githubData;
 // will prompt user for github username and to pick favorite color
 inquirer
     .prompt([{
@@ -34,33 +34,34 @@ inquirer
         //   using axios to retrieve info from github API
         axios.get(queryUrl).then(function (res) {
             const githubData = res.data;
-            console.log(githubData);
-            // axios.get(starsQueryUrl).then(function (starRes) {
-            //     const stars = starRes.data.length;
+        }).then(function (res) {
+            axios.get(starsQueryUrl).then(function (res) {
+                const stars = res.data.length;
                 // this logs the number showing up on page1...not sure how to get the TOTAL number of stars a user has
-                console.log("*got here*");
-                // console.log(stars);                         
-            
-            // writes HTML File
-            fs.writeFile('first.html', genHTML, function (err) {
-                if (err) throw err;
-                console.log('Saved HTML File');
 
-                      
 
-            // creates PDF file
-                const html = fs.readFileSync('./first.html', 'utf8');
-                const options = { format: 'Letter' };
+                // writes HTML File
+                console.log(genHTML(username, color));
+                fs.writeFile('first.html', genHTML(githubData, stars, color), function (err) {
+                    console.log(genHTML);
+                    if (err) throw err;
+                    console.log('Saved HTML File');
 
-                pdf.create(html, options).toFile('.devprofile.pdf', function (err, res) {
-                    if (err) return console.log(err);
-                    console.log(res);
 
+
+                    // creates PDF file
+                    const html = fs.readFileSync('./first.html', 'utf8');
+                    const options = { format: 'Letter' };
+
+                    pdf.create(html, options).toFile('.devprofile.pdf', function (err, res) {
+                        if (err) return console.log(err);
+                        console.log(res);
+
+                    });
                 });
             });
 
 
         });
     });
-        
-    // });
+
